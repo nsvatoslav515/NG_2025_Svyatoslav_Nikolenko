65e3b8e6cfb31f3960033c611bfe5ef46cdf45a4
nsvatoslav515 <nsvatoslav515@gmail.com>
2025-10-22 20:42:34 +0300

    Add files via upload

---
+++ Lesson_1/Task_2/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double number1, number2;
+    char action;
+
+    cout << "Enter one number: ";
+    cin >> number1;
+
+    cout << "Enter second number: ";
+    cin >> number2;
+
+    cout << "Enter action (+, -, *, /): ";
+    cin >> action;
+
+    cout << number1 << " " << action << " " << number2 << " = ";
+
+    if (action == '+') {
+        cout << number1 + number2;
+    }
+    else if (action == '-') {
+        cout << number1 - number2;
+    }
+    else if (action == '*') {
+        cout << number1 * number2;
+    }
+    else {
+        cout << number1 / number2;
+    }
+    return 0;
+}
