e9b81eaddec2cdcad70476e2bac6df8f100c495d
Silhouette18 <161963725+Silhouette18@users.noreply.github.com>
2025-10-24 18:25:53 +0300

    Update

--- Lesson_1/Task_2/main.cpp
+++ Lesson_1/Task_2/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+#include <cmath>
 
 int main() {
     double number1, number2;
@@ -11,7 +12,7 @@ int main() {
     cout << "Enter second number: ";
     cin >> number2;
 
-    cout << "Enter action (+, -, *, /): ";
+    cout << "Enter action (+, -, *, /, ^, s): ";
     cin >> action;
 
     cout << number1 << " " << action << " " << number2 << " = ";
@@ -30,6 +31,15 @@ int main() {
             cout << number1 / number2;
         } else {
             cout << "Error: Division by zero!";
+        }
+    } else if (action == '^') {
+        cout << pow(number1, number2);
+    } 
+    else if (action == 's' || action == 'S') {
+        if (number1 >= 0) {
+            cout << sqrt(number1);
+        } else {
+            cout << "Error: Cannot calculate square root of negative number!";
         }
     } else {
         cout << "Error: Invalid operator";
