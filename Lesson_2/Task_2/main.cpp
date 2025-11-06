791313bf4187326afb622be967ec5e389c3a61ab
Silhouette18 <161963725+Silhouette18@users.noreply.github.com>
2025-11-06 15:55:08 +0200

    update main.cpp

--- Lesson_2/Task_2/main.cpp
+++ Lesson_2/Task_2/main.cpp
@@ -1,37 +1,38 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-    int one, two, three, four, five;
-    cout << "Enter 5 numbers: " << endl;
-    cin >> one;
-    cin >> two;
-    cin >> three;
-    cin >> four;
-    cin >> five;
+    int size;
+    cout << "How many numbers? ";
+    cin >> size;
     
-    int array[5] = {one, two, three, four, five};
+    int array[size]; 
     
+    cout << "Enter " << size << " numbers: " << endl;
+    for (int counter = 0; counter < size; counter++) {
+        cin >> array[counter];
+    }
+    
+
     int max = 0;
-    
-    for (int counter = 0; counter < 5; counter++) {
-        if (array[counter] >= max) {
+    for (int counter = 0; counter < size; counter++) {
+        if (array[counter] > max) {
             max = array[counter];
         }
     }
     
-    
+    // Выводим график
     for (int row = 1; row <= max; row++) {
-        cout << "\n";
-        for (int columnIndex = 0; columnIndex < 5; columnIndex++) {
-            if (array[columnIndex] >= row) {
+        cout << endl;
+        for (int i = 0; i < size; i++) {
+            if (array[i] >= row) {
                 cout << '*';
             } else {
                 cout << ' ';
             }
         }
     }
+    
     return 0;
 }
