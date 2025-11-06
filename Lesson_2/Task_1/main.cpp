8325efebdea806269309bf0f0df78482fb87da61
Silhouette18 <161963725+Silhouette18@users.noreply.github.com>
2025-11-06 17:01:58 +0200

    Update

--- Lesson_2/Task_1/main.cpp
+++ Lesson_2/Task_1/main.cpp
@@ -4,54 +4,67 @@ using namespace std;
 
 int main()
 {
-    int accounts[10] = {1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000};
+    int accounts[10] = {0,0,0,0,0,0,0,0,0,1000};
     int choice;
+    
     cout << "What do you want?" << endl;
-    cout << "1.Information about account." << endl;
-    cout << "2.Sum of accounts: " << endl;
-    cout << "3.Withdraw money" << endl;
-    cout << "4.Deposit money" << endl;
+    cout << "1. Information about account." << endl;
+    cout << "2. Sum of accounts: " << endl;
+    cout << "3. Withdraw money" << endl;
+    cout << "4. Deposit money" << endl;
     
     cin >> choice;
     
-    if (choice == 4) {
-        int amount;
-        int accountNumber;
-        cout << "Enter account number: ";
-        cin >> accountNumber;
-        cout << "How much would you like to deposit?" << endl;
-        cin >> amount;
-        cout << "Completed! You deposited " << amount << " into account: " << accountNumber << endl;
-        cout << "New balance: " << accounts[accountNumber - 1] + amount;
-    }
-    
-    if (choice == 3) {
-        int amount;
-        int accountNumber;
-        cout << "Enter account number: ";
-        cin >> accountNumber;
-        cout << "How much would you like to withdraw?" << endl;
-        cin >> amount;
-        if (accounts[accountNumber - 1] >= amount) {
-            cout << "Money withdrawn: -" << amount << endl;
-            cout << "Remaining balance: " << accounts[accountNumber - 1] - amount << endl;
-        } else {
-            cout << "Insufficient funds in this account." << endl;
+    switch (choice) {
+        case 1: {
+            int accountNumber;
+            cout << "Enter account number: " << endl;
+            cin >> accountNumber;
+            cout << "Account balance: " << accounts[accountNumber - 1];
+            break;
         }
-    }
-    
-    if (choice == 1) {
-        int accountNumber;
-        cout << "Enter account number: " << endl;
-        cin >> accountNumber;
-        cout << "Account balance: " << accounts[accountNumber - 1];
-    }
-    
-    if (choice == 2) {
-        int sum = 0;
-        for (int counter = 0; counter < 10; counter++) {
-            sum += accounts[counter];
+        
+        case 2: {
+            int sum = 0;
+            for (int counter = 0; counter < 10; counter++) {
+                sum += accounts[counter];
+            }
+            cout << "Sum: " << sum << endl;
+            break;
         }
-        cout << "Sum: " << sum << endl;
+        
+        case 3: {
+            int amount;
+            int accountNumber;
+            cout << "Enter account number: ";
+            cin >> accountNumber;
+            cout << "How much would you like to withdraw?" << endl;
+            cin >> amount;
+            if (accounts[accountNumber - 1] >= amount) {
+                cout << "Money withdrawn: -" << amount << endl;
+                cout << "Remaining balance: " << accounts[accountNumber - 1] - amount << endl;
+            } else {
+                cout << "Insufficient funds in this account." << endl;
+            }
+            break;
+        }
+        
+        case 4: {
+            int amount;
+            int accountNumber;
+            cout << "Enter account number: ";
+            cin >> accountNumber;
+            cout << "How much would you like to deposit?" << endl;
+            cin >> amount;
+            cout << "Completed! You deposited " << amount << " into account: " << accountNumber << endl;
+            cout << "New balance: " << accounts[accountNumber - 1] + amount;
+            break;
+        }
+        
+        default:
+            cout << "Invalid choice!" << endl;
+            break;
     }
+    
+    return 0;
 }
